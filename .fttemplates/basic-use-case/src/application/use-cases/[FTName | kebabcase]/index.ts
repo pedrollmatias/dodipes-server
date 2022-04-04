29bcb9be81eeea71fd415890c2a01ed5c081c7f8
pedrollmatias <pedrollmatias@gmail.com>
2022-04-04 00:10:50 -0300

    fix: use case index

--- .fttemplates/basic-use-case/src/application/use-cases/[FTName | kebabcase]/index.ts
+++ .fttemplates/basic-use-case/src/application/use-cases/[FTName | kebabcase]/index.ts
@@ -1,6 +1,6 @@
 import { <FTName | pascalcase> } from './<FTName | kebabcase>.use-case';
 import { repository } from '../../../infraestructure/repositories/mongodb';
 
-export const <FTName | camelcase> = new <FTName | pascalcase>({
+export const <FTName | camelcase>UseCase = new <FTName | pascalcase>({
   repositories: { repository },
 });
